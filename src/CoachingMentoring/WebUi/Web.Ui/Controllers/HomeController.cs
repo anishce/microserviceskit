f38f797b7f5dbcc45f96ccbdd97b12ece6c947a1
Anish <anish.cse7@gmail.com>
2024-05-03 22:22:15 +0530

    Added Web UI project
    Added Web UI project

---
+++ src/CoachingMentoring/WebUi/Web.Ui/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using AnishCeDev.CoachingMentoring.Web.Ui.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace AnishCeDev.CoachingMentoring.Web.Ui.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ src/CoachingMentoring/WebUi/Web.Ui/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AnishCeDev.CoachingMentoring.Web.Ui.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
