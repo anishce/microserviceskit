5b9ae4191d96b62985564221bda6d90d54f92850
Anish CE <anish.cse7@gmail.com>
2024-08-31 14:59:13 +0530

    Added Feedback and Review Infrastructure project
    Added Feedback and Review Infrastructure project

---
+++ src/CoachingMentoring/FeedbackAndReview/Infrastructure/DomainBase/IEntity.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AnishCeDev.CoachingMentoring.FeedbackReview.Infra.DomainBase
+{
+    public interface IEntity
+    {
+    }
+}
