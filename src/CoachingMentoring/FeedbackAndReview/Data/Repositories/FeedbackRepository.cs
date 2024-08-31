33c163c95f96f2776152770950f83473a0a8db6e
Anish CE <anish.cse7@gmail.com>
2024-08-31 15:01:55 +0530

    Added Feedback Repository

---
+++ src/CoachingMentoring/FeedbackAndReview/Data/Repositories/FeedbackRepository.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AnishCeDev.CoachingMentoring.FeedbackReview.Data.Repositories
+{
+    internal class FeedbackRepository
+    {
+    }
+}
