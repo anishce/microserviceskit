afb74b7fb92775d4775ec043ca2bc3f233de739d
Anish CE <anish.cse7@gmail.com>
2024-08-31 15:02:12 +0530

    Added Feedback aggregate
    Added Feedback aggregate

---
+++ src/CoachingMentoring/FeedbackAndReview/Domain/Aggregates/Feedback.cs
@@ -0,0 +1,11 @@
+﻿using AnishCeDev.CoachingMentoring.FeedbackReview.Infra.DomainBase;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AnishCeDev.CoachingMentoring.FeedbackReview.Domain.Aggregates
+{
+    internal class Feedback : IEntity
+    {
+    }
+}
