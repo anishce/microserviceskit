eba61a8d20e8c8747b52d0d77a4e5277ae881848
Anish CE <anish.cse7@gmail.com>
2024-09-07 14:39:56 +0530

    Added Learning & Development Aggregates

---
+++ src/CoachingMentoring/LearningAndDevelopment/Domain/DevelopmentAggregates/Development.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AnishCeDev.CoachingMentoring.LearningAndDevelopment.Domain.DevelopmentAggregates
+{
+    internal class Development
+    {
+    }
+}
---
+++ src/CoachingMentoring/LearningAndDevelopment/Domain/LearningAggregates/Learning.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace AnishCeDev.CoachingMentoring.Coaching.Domain.LearningAggregates
+{
+    public class Learning
+    {
+
+    }
+}
