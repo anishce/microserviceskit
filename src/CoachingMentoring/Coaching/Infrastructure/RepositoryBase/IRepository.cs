4f28cbc5c24786ce3aee8977355ad73070e01f85
Anish CE <anish.cse7@gmail.com>
2024-11-24 11:51:50 +0530

    Create IRepository.cs

---
+++ src/CoachingMentoring/Coaching/Infrastructure/RepositoryBase/IRepository.cs
@@ -0,0 +1,11 @@
+﻿using AnishCeDev.CoachingMentoring.Coaching.Infra.DomainBase;
+
+namespace AnishCeDev.CoachingMentoring.Coaching.Infra.RepositoryBase
+{
+    public interface IRepository<T, TId> : IReadOnlyRepository<T, TId> where T : IAggregateRoot
+    {
+        void Save(T entity);
+        void Add(T entity);
+        void Remove(T entity);
+    }
+}
