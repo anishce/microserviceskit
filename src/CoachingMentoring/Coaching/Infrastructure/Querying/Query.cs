a063fdfff3bc9ec2f1b82d13e4d799f2ccb71d7b
Anish CE <anish.cse7@gmail.com>
2024-11-24 11:51:43 +0530

    Create Query.cs

---
+++ src/CoachingMentoring/Coaching/Infrastructure/Querying/Query.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace AnishCeDev.CoachingMentoring.Coaching.Infra.Querying
+{
+    public class Query
+    {
+        private IList<Query> _subQueries = new List<Query>();
+
+        public IEnumerable<Query> SubQueries
+        {
+            get { return _subQueries; }
+        }
+
+        public void AddSubQuery(Query subQuery)
+        {
+            _subQueries.Add(subQuery);
+        }
+
+    }
+}
