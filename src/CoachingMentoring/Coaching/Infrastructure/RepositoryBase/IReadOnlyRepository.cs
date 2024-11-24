1c8fdf02e913d7afa3ba111cefb4a8e66cd675f8
Anish CE <anish.cse7@gmail.com>
2024-11-24 11:51:47 +0530

    Create IReadOnlyRepository.cs

---
+++ src/CoachingMentoring/Coaching/Infrastructure/RepositoryBase/IReadOnlyRepository.cs
@@ -0,0 +1,14 @@
+﻿using AnishCeDev.CoachingMentoring.Coaching.Infra.DomainBase;
+using AnishCeDev.CoachingMentoring.Coaching.Infra.Querying;
+using System.Collections.Generic;
+
+namespace AnishCeDev.CoachingMentoring.Coaching.Infra.RepositoryBase
+{
+    public interface IReadOnlyRepository<T, TId> where T : IAggregateRoot 
+    {
+        T FindBy(TId id);
+        IEnumerable<T> FindAll();        
+        IEnumerable<T> FindBy(Query query);
+        IEnumerable<T> FindBy(Query query, int index, int count);
+    }
+}
