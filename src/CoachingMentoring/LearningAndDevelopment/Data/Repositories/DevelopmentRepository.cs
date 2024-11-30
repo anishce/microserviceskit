cf12ed7faff1ca6a1103e6b1808b3f7d095752f7
Anish CE <anish.cse7@gmail.com>
2024-11-30 18:56:15 +0530

    Create DevelopmentRepository.cs

---
+++ src/CoachingMentoring/LearningAndDevelopment/Data/Repositories/DevelopmentRepository.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AnishCeDev.CoachingMentoring.LearningAndDevelopment.Data.Repositories
+{
+    internal class DevelopmentRepository
+    {
+    }
+}
