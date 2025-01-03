7a1be47c32d22a509d0d8c2d0b503b71d8cd2c08
Anish CE <anish.cse7@gmail.com>
2025-01-03 22:11:07 +0530

    Create Priority.cs

---
+++ src/TaskManagement/TaskService/Domain/PriorityAggregate/Priority.cs
@@ -0,0 +1,32 @@
+﻿using AnishCeDev.TaskManagement.Domain.Shared;
+using AnishCeDev.TaskManagement.Task.Infra.DomainBase;
+
+namespace AnishCeDev.TaskManagement.Task.Domain.PriorityAggregate
+{
+    public class Priority : BaseEntity<int>, IAggregateRoot
+    {
+        public Priority(int id, string name, int ordinal, IEnumerable<Link> links, int version) : base(id, version)
+        {
+            Name = name;
+            Ordinal = ordinal;
+            Links = links;
+        }
+
+        public string Name { get; private set; } = null!;
+        public int? Ordinal { get; private set; }
+        public IEnumerable<Link> Links { get; private set; } = null!;
+
+        protected override void ValidateEntity()
+        {
+            if (string.IsNullOrWhiteSpace(Name))
+            {
+                throw new ArgumentNullException("Name");
+            }
+
+            if (Links is null || Links?.Count() <= 0)
+            {
+                throw new ArgumentNullException("Links");
+            }
+        }
+    }
+}
