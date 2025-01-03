c4b79c91e23cd88ba66fd6ce891d5399b37a869b
Anish CE <anish.cse7@gmail.com>
2025-01-03 22:02:50 +0530

    Create RepositoryBase.cs

---
+++ src/TaskManagement/TaskService/Infra/RepositoryBase/RepositoryBase.cs
@@ -0,0 +1,20 @@
+﻿using AnishCeDev.TaskManagement.Task.Infra.DomainBase;
+using AnishCeDev.TaskManagement.Task.Infra.Querying;
+using System.Collections.Generic;
+
+namespace AnishCeDev.TaskManagement.Task.Infra.RepositoryBase
+{
+    public abstract class RepositoryBase<T, TId> : IRepository<T, TId>  where T : IAggregateRoot
+    {
+        #region IRepository
+        public abstract void Add(T entity);
+        public abstract void Save(T entity);
+        public abstract void Remove(T entity);
+
+        public abstract T FindBy(TId id);
+        public abstract IEnumerable<T> FindBy(Query query);
+        public abstract IEnumerable<T> FindBy(Query query, int index, int count);
+        public abstract IEnumerable<T> FindAll();
+        #endregion
+    }
+}
