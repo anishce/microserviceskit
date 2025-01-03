3e29000d1e6bb42f29b83fb32233b7ff68142a08
Anish CE <anish.cse7@gmail.com>
2025-01-03 22:07:50 +0530

    Create TaskRepository.cs

---
+++ src/TaskManagement/TaskService/Data/Repositories/TaskRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnishCeDev.TaskManagement.Task.Data.Repositories
+{
+    internal class TaskRepository
+    {
+    }
+}
