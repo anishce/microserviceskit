400efa9d4b73f2e80c2234b9998a3799ca53286e
Anish CE <anish.cse7@gmail.com>
2025-01-03 22:11:05 +0530

    Create Category.cs

---
+++ src/TaskManagement/TaskService/Domain/CategoryAggregate/Category.cs
@@ -0,0 +1,32 @@
+﻿using AnishCeDev.TaskManagement.Domain.Shared;
+using AnishCeDev.TaskManagement.Task.Infra.DomainBase;
+
+namespace AnishCeDev.TaskManagement.Task.Domain.CategoryAggregate
+{
+    public class Category : BaseEntity<int>, IAggregateRoot
+    {
+        public Category(int id, string name, string description, IEnumerable<Link> links, int version) : base(id, version)
+        {
+            Name = name;
+            Description = description;
+            Links = links;
+        }
+
+        public string Name { get; private set; } = null!;
+        public string? Description { get; private set; } = null;
+        public IEnumerable<Link> Links { get; private set; } = null!;
+
+        protected override void ValidateEntity()
+        {
+            if (string.IsNullOrWhiteSpace(Name))
+            {
+                throw new ArgumentNullException("Name");
+            }
+
+            if (Links is null || Links?.Count() <= 0)
+            {
+                throw new ArgumentNullException("Links");
+            }
+        }
+    }
+}
