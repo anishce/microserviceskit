a43a8c67f8235e5fcdbd5268298f004281599801
Anish CE <anish.cse7@gmail.com>
2025-01-03 22:02:35 +0530

    Create IAggregateRoot.cs

---
+++ src/TaskManagement/TaskService/Infra/DomainBase/IAggregateRoot.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AnishCeDev.TaskManagement.Task.Infra.DomainBase
+{
+    public interface IAggregateRoot
+    {
+    }
+}
