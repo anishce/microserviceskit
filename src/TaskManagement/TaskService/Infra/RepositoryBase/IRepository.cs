466649a23bd0257d049cd550affa85fc135b3e84
Anish CE <anish.cse7@gmail.com>
2025-01-03 22:02:47 +0530

    Create IRepository.cs

---
+++ src/TaskManagement/TaskService/Infra/RepositoryBase/IRepository.cs
@@ -0,0 +1,11 @@
+﻿using AnishCeDev.TaskManagement.Task.Infra.DomainBase;
+
+namespace AnishCeDev.TaskManagement.Task.Infra.RepositoryBase
+{
+    public interface IRepository<T, TId> : IReadOnlyRepository<T, TId> where T : IAggregateRoot
+    {
+        void Save(T entity);
+        void Add(T entity);
+        void Remove(T entity);
+    }
+}
