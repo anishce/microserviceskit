73c743256744bcc16dc42eb8d0f19c9e4c5be1e3
Anish CE <anish.cse7@gmail.com>
2025-01-04 15:36:28 +0530

    Create ICategoryAppService.cs

---
+++ src/TaskManagement/TaskService/Web.Api/ApplicationServices/ICategoryAppService.cs
@@ -0,0 +1,20 @@
+﻿// ************************************************************************
+// Copyright (c) AnishCeDev All Rights Reserved.
+// Author: AnishCeDev
+// ************************************************************************
+
+using AnishCeDev.TaskManagement.Task.Web.Api.Models;
+
+namespace AnishCeDev.TaskManagement.Task.Web.Api.ApplicationServices
+{
+    public interface ICategoryAppService
+    {
+        System.Threading.Tasks.Task AddCategoryAsync(CategoryModel category);
+        Task<CategoryModel> GetCategoryAsync(int categoryId);
+        Task<IEnumerable<CategoryModel>> GetCategoriesAsync();
+        System.Threading.Tasks.Task RemoveCategoriesAsync(IEnumerable<int> categoryIds);
+        System.Threading.Tasks.Task RemoveCategoryAsync(int categoryId);
+        System.Threading.Tasks.Task UpdateCategoriesAsync(IEnumerable<CategoryModel> categories);
+        System.Threading.Tasks.Task UpdateCategoryAsync(CategoryModel category);
+    }
+}
