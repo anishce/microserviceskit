03fc4a0ef0130ef250cd8ab309a1a72aa8052e58
Anish CE <anish.cse7@gmail.com>
2025-01-08 21:44:24 +0530

    Create UsersController.cs

---
+++ src/TaskManagement/TaskService/Web.Api/Controllers/UsersController.cs
@@ -0,0 +1,65 @@
+﻿// ************************************************************************
+// Copyright (c) AnishCeDev All Rights Reserved.
+// Author: AnishCeDev
+// ************************************************************************
+
+using AnishCeDev.TaskManagement.Task.Web.Api.ApplicationServices;
+using AnishCeDev.TaskManagement.Task.Web.Api.Models;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace AnishCeDev.TaskManagement.Task.Web.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly IUserAppService userAppService;
+        public UsersController(IUserAppService userAppService)
+        {
+            this.userAppService = userAppService;
+        }
+
+        // GET: api/<UsersController>
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var users = await this.userAppService.GetUsersAsync();
+
+            return Ok(users);
+        }
+
+        // GET api/<UsersController>/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(int id)
+        {
+            var user = await this.userAppService.GetUserAsync(id);
+
+            return Ok(user);
+        }
+
+        // POST api/<UsersController>
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] UserModel user)
+        {
+            if (user is null || !ModelState.IsValid)
+            {
+                return BadRequest("Invalid user create request.");
+            }
+            return Ok();
+        }
+
+        // PUT api/<UsersController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<UsersController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
