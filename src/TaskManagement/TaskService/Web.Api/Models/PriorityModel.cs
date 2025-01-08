8fdc5128df9980c1ee134d1acac00a68407fa523
Anish CE <anish.cse7@gmail.com>
2025-01-08 21:45:02 +0530

    Create PriorityModel.cs

---
+++ src/TaskManagement/TaskService/Web.Api/Models/PriorityModel.cs
@@ -0,0 +1,19 @@
+﻿// ************************************************************************
+// Copyright (c) AnishCeDev All Rights Reserved.
+// Author: AnishCeDev
+// ************************************************************************
+
+#pragma warning disable VSSpell001 // Spell Check
+#pragma warning disable VSSpell001 // Spell Check
+namespace AnishCeDev.TaskManagement.Task.Web.Api.Models
+#pragma warning restore VSSpell001 // Spell Check
+#pragma warning restore VSSpell001 // Spell Check
+{
+    public class PriorityModel
+    {
+        public long? PriorityId { get; set; } = null!;
+        public string Name { get; set; } = null!;
+        public int Ordinal { get; set; }
+        public List<LinkModel> Links { get; set; } = null!;
+    }
+}
