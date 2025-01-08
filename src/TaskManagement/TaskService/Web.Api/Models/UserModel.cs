f314fcab5b0c9f396eb81eed4bd26649cc75cca3
Anish CE <anish.cse7@gmail.com>
2025-01-08 21:51:53 +0530

    Create UserModel.cs

---
+++ src/TaskManagement/TaskService/Web.Api/Models/UserModel.cs
@@ -0,0 +1,27 @@
+﻿// ************************************************************************
+// Copyright (c) AnishCeDev All Rights Reserved.
+// Author: AnishCeDev
+// ************************************************************************
+
+#pragma warning disable VSSpell001 // Spell Check
+#pragma warning disable VSSpell001 // Spell Check
+namespace AnishCeDev.TaskManagement.Task.Web.Api.Models
+#pragma warning restore VSSpell001 // Spell Check
+#pragma warning restore VSSpell001 // Spell Check
+{
+    public class UserModel
+    {
+        public long UserId { get; set; }
+#pragma warning disable VSSpell001 // Spell Check
+        public string Username { get; set; } = null!;
+#pragma warning restore VSSpell001 // Spell Check
+#pragma warning disable VSSpell001 // Spell Check
+        public string Firstname { get; set; } = null!;
+#pragma warning restore VSSpell001 // Spell Check
+#pragma warning disable VSSpell001 // Spell Check
+        public string Lastname { get; set; } = null!;
+#pragma warning restore VSSpell001 // Spell Check
+        public string Email { get; set; } = null!;
+        public List<LinkModel> Links { get; set; } = null!;
+    }
+}
