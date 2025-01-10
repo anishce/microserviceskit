dc23d72bf8c30186fcd4749f5f7210febce1083f
Anish CE <anish.cse7@gmail.com>
2025-01-10 21:28:06 +0530

    Update BaseValueObject.cs

--- src/TaskManagement/TaskService/Infra/DomainBase/BaseValueObject.cs
+++ src/TaskManagement/TaskService/Infra/DomainBase/BaseValueObject.cs
@@ -1,12 +1,15 @@
-﻿using System.Collections.Generic;
+﻿// ************************************************************************
+// Copyright (c) AnishCeDev All Rights Reserved.
+// Author: AnishCeDev
+// ************************************************************************
 
 namespace AnishCeDev.TaskManagement.Task.Infra.DomainBase
 {
-    public abstract class BaseValueObject<T> where T: BaseValueObject<T>
+    public abstract class BaseValueObject<T> where T : BaseValueObject<T>
     {
         public BaseValueObject()
         {
-        
+
         }
 
         protected abstract IEnumerable<object> GetAttributesToBeCheckForEquality();
