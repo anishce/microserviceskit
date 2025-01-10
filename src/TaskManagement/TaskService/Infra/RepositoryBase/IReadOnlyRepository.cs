2441a656d5e6440b9bd8dfbce556a8b51d52fd39
Anish CE <anish.cse7@gmail.com>
2025-01-10 23:43:28 +0530

    Update IReadOnlyRepository.cs

--- src/TaskManagement/TaskService/Infra/RepositoryBase/IReadOnlyRepository.cs
+++ src/TaskManagement/TaskService/Infra/RepositoryBase/IReadOnlyRepository.cs
@@ -1,13 +1,17 @@
-﻿using AnishCeDev.TaskManagement.Task.Infra.DomainBase;
+﻿// ************************************************************************
+// Copyright (c) AnishCeDev All Rights Reserved.
+// Author: AnishCeDev
+// ************************************************************************
+
+using AnishCeDev.TaskManagement.Task.Infra.DomainBase;
 using AnishCeDev.TaskManagement.Task.Infra.Querying;
-using System.Collections.Generic;
 
 namespace AnishCeDev.TaskManagement.Task.Infra.RepositoryBase
 {
-    public interface IReadOnlyRepository<T, TId> where T : IAggregateRoot 
+    public interface IReadOnlyRepository<T, TId> where T : IAggregateRoot
     {
         T FindBy(TId id);
-        IEnumerable<T> FindAll();        
+        IEnumerable<T> FindAll();
         IEnumerable<T> FindBy(Query query);
         IEnumerable<T> FindBy(Query query, int index, int count);
     }
