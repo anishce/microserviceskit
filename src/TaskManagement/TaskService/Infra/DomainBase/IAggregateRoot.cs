7fc3d0c579cd8102c20eb1a5e635fa71952b26f1
Anish CE <anish.cse7@gmail.com>
2025-01-10 21:28:09 +0530

    Update IAggregateRoot.cs

--- src/TaskManagement/TaskService/Infra/DomainBase/IAggregateRoot.cs
+++ src/TaskManagement/TaskService/Infra/DomainBase/IAggregateRoot.cs
@@ -1,6 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿// ************************************************************************
+// Copyright (c) AnishCeDev All Rights Reserved.
+// Author: AnishCeDev
+// ************************************************************************
 
 namespace AnishCeDev.TaskManagement.Task.Infra.DomainBase
 {
