0cf992095f3b5f40666f7e3d8b679429f3e2cc9c
Anish CE <anish.cse7@gmail.com>
2025-01-11 16:14:24 +0530

    Update TaskRepository.cs

--- src/TaskManagement/TaskService/Data/Repositories/TaskRepository.cs
+++ src/TaskManagement/TaskService/Data/Repositories/TaskRepository.cs
@@ -1,12 +1,48 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿// ************************************************************************
+// Copyright (c) AnishCeDev All Rights Reserved.
+// Author: AnishCeDev
+// ************************************************************************
+
+using AnishCeDev.TaskManagement.Task.Infra.Querying;
+using AnishCeDev.TaskManagement.Task.Infra.RepositoryBase;
 
 namespace AnishCeDev.TaskManagement.Task.Data.Repositories
 {
-    internal class TaskRepository
+    internal class TaskRepository : RepositoryBase<AnishCeDev.TaskManagement.Task.Domain.TaskAggregate.Task, int>
     {
+        public override void Add(Domain.TaskAggregate.Task entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override IEnumerable<Domain.TaskAggregate.Task> FindAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Domain.TaskAggregate.Task FindBy(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override IEnumerable<Domain.TaskAggregate.Task> FindBy(Query query)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override IEnumerable<Domain.TaskAggregate.Task> FindBy(Query query, int index, int count)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Remove(Domain.TaskAggregate.Task entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Save(Domain.TaskAggregate.Task entity)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
