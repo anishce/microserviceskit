ea3db72c8e563d875a8fd953b9c466e1e77395fe
Anish CE <anish.cse7@gmail.com>
2025-07-05 13:24:28 +0530

    Create BaseEntity.cs

---
+++ src/TaskManagement/UserService/Infra/DomainBase/BaseEntity.cs
@@ -0,0 +1,22 @@
+﻿// ************************************************************************
+// Copyright (c) 2025 AnishCeDev All Rights Reserved.
+// Author: AnishCeDev
+// ************************************************************************
+
+namespace AnishCeDev.TaskManagement.Task.Infra.DomainBase
+{
+    public abstract class BaseEntity<TId> : IEntity
+    {
+        public BaseEntity(TId id, int version)
+        {
+            Id = id;
+            Version = version;
+        }
+
+        public TId Id { get; private set; } = default!;
+
+        public int Version { get; private set; }
+
+        protected abstract void ValidateEntity();
+    }
+}
