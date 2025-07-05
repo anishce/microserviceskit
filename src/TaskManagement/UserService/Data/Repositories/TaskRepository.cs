710107eae1336e3c9880a09d38e71bb50ff6e3c4
Anish CE <anish.cse7@gmail.com>
2025-07-05 13:24:08 +0530

    Create TaskRepository.cs

---
+++ src/TaskManagement/UserService/Data/Repositories/TaskRepository.cs
@@ -0,0 +1,48 @@
+﻿// ************************************************************************
+// Copyright (c) 2025 AnishCeDev All Rights Reserved.
+// Author: AnishCeDev
+// ************************************************************************
+
+using AnishCeDev.TaskManagement.Task.Infra.Querying;
+using AnishCeDev.TaskManagement.Task.Infra.RepositoryBase;
+
+namespace AnishCeDev.TaskManagement.Task.Data.Repositories
+{
+    internal class TaskRepository : RepositoryBase<AnishCeDev.TaskManagement.Task.Domain.TaskAggregate.Task, int>
+    {
+        public override void Add(Domain.TaskAggregate.Task entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override IEnumerable<Domain.TaskAggregate.Task> FindAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Domain.TaskAggregate.Task FindBy(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override IEnumerable<Domain.TaskAggregate.Task> FindBy(Query query)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override IEnumerable<Domain.TaskAggregate.Task> FindBy(Query query, int index, int count)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Remove(Domain.TaskAggregate.Task entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Save(Domain.TaskAggregate.Task entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
