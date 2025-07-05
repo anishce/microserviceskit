1ace7e5116071b48c493170208d64c92736a2fbe
Anish CE <anish.cse7@gmail.com>
2025-07-05 13:24:31 +0530

    Create BaseValueObject.cs

---
+++ src/TaskManagement/UserService/Infra/DomainBase/BaseValueObject.cs
@@ -0,0 +1,19 @@
+﻿// ************************************************************************
+// Copyright (c) 2025 AnishCeDev All Rights Reserved.
+// Author: AnishCeDev
+// ************************************************************************
+
+namespace AnishCeDev.TaskManagement.Task.Infra.DomainBase
+{
+    public abstract class BaseValueObject<T> where T : BaseValueObject<T>
+    {
+        public BaseValueObject()
+        {
+
+        }
+
+        protected abstract IEnumerable<object> GetAttributesToBeCheckForEquality();
+
+        protected abstract void Validate();
+    }
+}
