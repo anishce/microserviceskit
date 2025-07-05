3a338b1e82f920d04d59de224c51484ee8a2fdc9
Anish CE <anish.cse7@gmail.com>
2025-07-05 13:24:37 +0530

    Create IEntity.cs

---
+++ src/TaskManagement/UserService/Infra/DomainBase/IEntity.cs
@@ -0,0 +1,11 @@
+﻿// ************************************************************************
+// Copyright (c) 2025 AnishCeDev All Rights Reserved.
+// Author: AnishCeDev
+// ************************************************************************
+
+namespace AnishCeDev.TaskManagement.Task.Infra.DomainBase
+{
+    public interface IEntity
+    {
+    }
+}
