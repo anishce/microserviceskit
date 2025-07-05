44679d797afdd9d966823c68b5ab85ba25ed94ce
Anish CE <anish.cse7@gmail.com>
2025-07-05 13:24:40 +0530

    Create Query.cs

---
+++ src/TaskManagement/UserService/Infra/Querying/Query.cs
@@ -0,0 +1,23 @@
+﻿// ************************************************************************
+// Copyright (c) 2025 AnishCeDev All Rights Reserved.
+// Author: AnishCeDev
+// ************************************************************************
+
+namespace AnishCeDev.TaskManagement.Task.Infra.Querying
+{
+    public class Query
+    {
+        private IList<Query> _subQueries = new List<Query>();
+
+        public IEnumerable<Query> SubQueries
+        {
+            get { return _subQueries; }
+        }
+
+        public void AddSubQuery(Query subQuery)
+        {
+            _subQueries.Add(subQuery);
+        }
+
+    }
+}
