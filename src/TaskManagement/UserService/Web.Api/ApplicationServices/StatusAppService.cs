4d440fd3bb5963a2e1d953692b4b6bde09b8a326
Anish CE <anish.cse7@gmail.com>
2025-07-07 20:58:08 +0530

    Create StatusAppService.cs

---
+++ src/TaskManagement/UserService/Web.Api/ApplicationServices/StatusAppService.cs
@@ -0,0 +1,72 @@
+﻿// ************************************************************************
+// Copyright (c) 2025 AnishCeDev All Rights Reserved.
+// Author: AnishCeDev
+// ************************************************************************
+
+using AnishCeDev.TaskManagement.Task.Web.Api.Models;
+
+#pragma warning disable VSSpell001 // Spell Check
+#pragma warning disable VSSpell001 // Spell Check
+namespace AnishCeDev.TaskManagement.Task.Web.Api.ApplicationServices
+#pragma warning restore VSSpell001 // Spell Check
+#pragma warning restore VSSpell001 // Spell Check
+{
+#pragma warning disable VSSpell001 // Spell Check
+    public class StatusAppService : IStatusAppService
+#pragma warning restore VSSpell001 // Spell Check
+    {
+        public async Task<StatusModel> GetStatusAsync(int statusId)
+        {
+            return await System.Threading.Tasks.Task.Run(() => GetStatusModel());
+        }
+
+        public async Task<IEnumerable<StatusModel>> GetStatusesAsync()
+        {
+            return await System.Threading.Tasks.Task.Run(() => GetStatusList());
+        }
+
+
+        private StatusModel GetStatusModel()
+        {
+            return new StatusModel
+            {
+                StatusId = 1,
+                Ordinal = 2,
+                Name = "In Progress",
+                Links = new List<LinkModel>
+                {
+                    new LinkModel
+                    {
+                        Href="https://www.google.com",
+                        Rel="",
+                        Title="Google",
+                        Type="HyperLink"
+                    }
+                }
+            };
+        }
+
+        private IEnumerable<StatusModel> GetStatusList()
+        {
+            return new List<StatusModel>
+            {
+                new StatusModel
+                {
+                    StatusId = 1,
+                    Ordinal = 2,
+                    Name = "In Progress",
+                    Links = new List<LinkModel>
+                    {
+                        new LinkModel
+                        {
+                            Href="https://www.google.com",
+                            Rel="",
+                            Title="Google",
+                            Type="HyperLink"
+                        }
+                    }
+                }
+            };
+        }
+    }
+}
