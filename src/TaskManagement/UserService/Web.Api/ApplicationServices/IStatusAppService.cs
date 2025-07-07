9353337ff25c4b92e2aa9a7d3d838236a82f1c16
Anish CE <anish.cse7@gmail.com>
2025-07-07 20:57:57 +0530

    Create IStatusAppService.cs

---
+++ src/TaskManagement/UserService/Web.Api/ApplicationServices/IStatusAppService.cs
@@ -0,0 +1,21 @@
+﻿// ************************************************************************
+// Copyright (c) 2025 AnishCeDev All Rights Reserved.
+// Author: AnishCeDev
+// ************************************************************************
+
+using AnishCeDev.TaskManagement.Task.Web.Api.Models;
+
+#pragma warning disable VSSpell001 // Spell Check
+#pragma warning disable VSSpell001 // Spell Check
+namespace AnishCeDev.TaskManagement.Task.Web.Api.ApplicationServices
+#pragma warning restore VSSpell001 // Spell Check
+#pragma warning restore VSSpell001 // Spell Check
+{
+#pragma warning disable VSSpell001 // Spell Check
+    public interface IStatusAppService
+#pragma warning restore VSSpell001 // Spell Check
+    {
+        Task<StatusModel> GetStatusAsync(int statusId);
+        Task<IEnumerable<StatusModel>> GetStatusesAsync();
+    }
+}
