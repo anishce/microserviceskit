977b0dac9ed224ea93785779f5edc77e651e0764
Anish CE <anish.cse7@gmail.com>
2025-07-07 20:57:59 +0530

    Create ITaskAppService.cs

---
+++ src/TaskManagement/UserService/Web.Api/ApplicationServices/ITaskAppService.cs
@@ -0,0 +1,16 @@
+﻿// ************************************************************************
+// Copyright (c) 2025 AnishCeDev All Rights Reserved.
+// Author: AnishCeDev
+// ************************************************************************
+
+using AnishCeDev.TaskManagement.Task.Web.Api.Models;
+
+namespace AnishCeDev.TaskManagement.Task.Web.Api.ApplicationServices
+{
+    public interface ITaskAppService
+    {
+        System.Threading.Tasks.Task AddNewTaskAsync(TaskModel task);
+        Task<TaskModel> GetTaskAsync(int taskId);
+        System.Threading.Tasks.Task UpdateTaskAsync(TaskModel task);
+    }
+}
