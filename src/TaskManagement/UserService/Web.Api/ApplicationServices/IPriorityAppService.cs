c9dd72e9f068ed5dae8a9ec04aa7963123f3d771
Anish CE <anish.cse7@gmail.com>
2025-07-07 20:57:54 +0530

    Create IPriorityAppService.cs

---
+++ src/TaskManagement/UserService/Web.Api/ApplicationServices/IPriorityAppService.cs
@@ -0,0 +1,21 @@
+﻿// ************************************************************************
+// Copyright (c) 2025 AnishCeDev All Rights Reserved.
+// Author: AnishCeDev
+// ************************************************************************
+
+using AnishCeDev.TaskManagement.Task.Web.Api.Models;
+
+#pragma warning disable VSSpell001 // Spell Check
+#pragma warning disable VSSpell001 // Spell Check
+namespace AnishCeDev.TaskManagement.Task.Web.Api.ApplicationServices
+#pragma warning restore VSSpell001 // Spell Check
+#pragma warning restore VSSpell001 // Spell Check
+{
+#pragma warning disable VSSpell001 // Spell Check
+    public interface IPriorityAppService
+#pragma warning restore VSSpell001 // Spell Check
+    {
+        Task<PriorityModel> GetPriorityAsync(int priorityId);
+        Task<IEnumerable<PriorityModel>> GetPrioritiesAsync();
+    }
+}
