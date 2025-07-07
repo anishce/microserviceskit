b5021ac0b0c8ed21f11c95f83d9fa84f6b6dd1ce
Anish CE <anish.cse7@gmail.com>
2025-07-07 20:57:35 +0530

    Create IReadOnlyRepository.cs

---
+++ src/TaskManagement/UserService/Infra/RepositoryBase/IReadOnlyRepository.cs
@@ -0,0 +1,18 @@
+﻿// ************************************************************************
+// Copyright (c) 2025 AnishCeDev All Rights Reserved.
+// Author: AnishCeDev
+// ************************************************************************
+
+using AnishCeDev.TaskManagement.Task.Infra.DomainBase;
+using AnishCeDev.TaskManagement.Task.Infra.Querying;
+
+namespace AnishCeDev.TaskManagement.Task.Infra.RepositoryBase
+{
+    public interface IReadOnlyRepository<T, TId> where T : IAggregateRoot
+    {
+        T FindBy(TId id);
+        IEnumerable<T> FindAll();
+        IEnumerable<T> FindBy(Query query);
+        IEnumerable<T> FindBy(Query query, int index, int count);
+    }
+}
