eff822f35fa8c803a28dc7a7e1a99fa44749e2b9
Anish CE <anish.cse7@gmail.com>
2025-07-08 21:23:45 +0530

    Create LinkModel.cs

---
+++ src/TaskManagement/UserService/Web.Api/Models/LinkModel.cs
@@ -0,0 +1,21 @@
+﻿// ************************************************************************
+// Copyright (c) 2025 AnishCeDev All Rights Reserved.
+// Author: AnishCeDev
+// ************************************************************************
+
+#pragma warning disable VSSpell001 // Spell Check
+#pragma warning disable VSSpell001 // Spell Check
+namespace AnishCeDev.TaskManagement.Task.Web.Api.Models
+#pragma warning restore VSSpell001 // Spell Check
+#pragma warning restore VSSpell001 // Spell Check
+{
+    public class LinkModel
+    {
+        public string Rel { get; set; } = null!;
+#pragma warning disable VSSpell001 // Spell Check
+        public string Href { get; set; } = null!;
+#pragma warning restore VSSpell001 // Spell Check
+        public string Title { get; set; } = null!;
+        public string Type { get; set; } = null!;
+    }
+}
