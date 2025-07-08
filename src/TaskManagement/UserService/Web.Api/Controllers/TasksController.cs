b2f7bac2e2b7aabc48ec40a92d1cf8a364d14d4a
Anish CE <anish.cse7@gmail.com>
2025-07-08 21:23:35 +0530

    Create TasksController.cs

---
+++ src/TaskManagement/UserService/Web.Api/Controllers/TasksController.cs
@@ -0,0 +1,61 @@
+﻿// ************************************************************************
+// Copyright (c) 2025 AnishCeDev All Rights Reserved.
+// Author: AnishCeDev
+// ************************************************************************
+
+using AnishCeDev.TaskManagement.Task.Web.Api.ApplicationServices;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace AnishCeDev.TaskManagement.Task.Web.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TasksController : ControllerBase
+    {
+        private readonly ITaskAppService taskAppService;
+        public TasksController(ITaskAppService taskAppService)
+        {
+            this.taskAppService = taskAppService;
+        }
+
+        // GET: api/<TasksController>
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            return Ok(new string[] { "value1", "value2" }.ToList());
+        }
+
+        // GET api/<TasksController>/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(int id)
+        {
+            if (id <= 0)
+            {
+                return BadRequest("Invalid Task Id.");
+            }
+            var task = await taskAppService.GetTaskAsync(id);
+            return Ok(task);
+        }
+
+        // POST api/<TasksController>
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] string value)
+        {
+            return Ok();
+        }
+
+        // PUT api/<TasksController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<TasksController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
