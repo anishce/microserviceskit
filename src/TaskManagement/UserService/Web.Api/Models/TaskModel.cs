62b6a0ebecd7779874c8bdbe1914682f5a6be6f0
Anish CE <anish.cse7@gmail.com>
2025-07-08 21:23:52 +0530

    Create TaskModel.cs

---
+++ src/TaskManagement/UserService/Web.Api/Models/TaskModel.cs
@@ -0,0 +1,25 @@
+﻿// ************************************************************************
+// Copyright (c) 2025 AnishCeDev All Rights Reserved.
+// Author: AnishCeDev
+// ************************************************************************
+
+#pragma warning disable VSSpell001 // Spell Check
+#pragma warning disable VSSpell001 // Spell Check
+namespace AnishCeDev.TaskManagement.Task.Web.Api.Models
+#pragma warning restore VSSpell001 // Spell Check
+#pragma warning restore VSSpell001 // Spell Check
+{
+    public class TaskModel
+    {
+        public long TaskId { get; set; }
+        public string Subject { get; set; } = null!;
+        public DateTime? StartDate { get; set; }
+        public DateTime? DueDate { get; set; }
+        public DateTime? DateCompleted { get; set; }
+        public List<CategoryModel> Categories { get; set; } = null!;
+        public PriorityModel Priority { get; set; } = null!;
+        public StatusModel Status { get; set; } = null!;
+        public List<LinkModel> Links { get; set; } = null!;
+        public List<UserModel> Assignees { get; set; } = null!;
+    }
+}
