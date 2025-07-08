9428966e0a7ed3901ad97d50d4828637c501d227
Anish CE <anish.cse7@gmail.com>
2025-07-08 21:23:32 +0530

    Create StatusesController.cs

---
+++ src/TaskManagement/UserService/Web.Api/Controllers/StatusesController.cs
@@ -0,0 +1,61 @@
+﻿// ************************************************************************
+// Copyright (c) 2025 AnishCeDev All Rights Reserved.
+// Author: AnishCeDev
+// ************************************************************************
+
+using AnishCeDev.TaskManagement.Task.Web.Api.ApplicationServices;
+using Microsoft.AspNetCore.Mvc;
+
+#pragma warning disable VSSpell001 // Spell Check
+#pragma warning disable VSSpell001 // Spell Check
+namespace AnishCeDev.TaskManagement.Task.Web.Api.Controllers
+#pragma warning restore VSSpell001 // Spell Check
+#pragma warning restore VSSpell001 // Spell Check
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class StatusesController : ControllerBase
+    {
+        private readonly IStatusAppService statusAppService;
+        private readonly ILogger logger;
+#pragma warning disable VSSpell001 // Spell Check
+        public StatusesController(IStatusAppService statusAppService, ILogger logger)
+#pragma warning restore VSSpell001 // Spell Check
+        {
+            this.statusAppService = statusAppService;
+            this.logger = logger;
+        }
+
+        // GET: api/status/1
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(int id)
+        {
+            try
+            {
+                var status = await this.statusAppService.GetStatusAsync(id);
+                return Ok(status);
+            }
+            catch (Exception ex)
+            {
+                logger.Log(LogLevel.Error, ex, ex.Message);
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
+        }
+
+        // GET: api/status
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            try
+            {
+                var statuses = await this.statusAppService.GetStatusesAsync();
+                return Ok(statuses);
+            }
+            catch (Exception ex)
+            {
+                logger.Log(LogLevel.Error, ex, ex.Message);
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
+        }
+    }
+}
